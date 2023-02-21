684663a21ad57ab5f9bdeac70cb73e0a9c3d75f8
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2023-02-21 13:02:36 +0200

    removed comments, refactored styles naming

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -23,7 +23,6 @@ export class FormComponent implements OnInit {
   feOptions = ['angular', 'react', 'vue'];
 
   constructor(private fb: FormBuilder, public formService: FormService) {
-    // set reactive form with default values and validators
     this.feForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -45,16 +44,14 @@ export class FormComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // subscribe for framework control changes
-    // enable frameworkVersions control get the selected framework versions
-    this.framework.valueChanges.pipe(distinctUntilChanged())
+    this.framework.valueChanges
+      .pipe(distinctUntilChanged())
       .subscribe((value) => {
         this.feForm.controls['frameworkVersion'].enable();
         this.formService.getFrameworkVersions(value);
       });
   }
 
-  // provide the form value and reset
   onSubmit(formDirective: FormGroupDirective): void {
     console.log({
       ...this.feForm.value,
@@ -64,7 +61,6 @@ export class FormComponent implements OnInit {
     this.feForm.reset();
   }
 
-  // add a new control group to the hobbies form array
   onAddHobby(): void {
     (<FormArray>this.feForm.get('hobbies')).push(
       this.fb.group({
@@ -73,17 +69,17 @@ export class FormComponent implements OnInit {
       })
     );
   }
-  // remove the selected control group from the hobbies form array
+
   onDeleteHobby(i: number): void {
     (<FormArray>this.feForm.get('hobbies')).removeAt(i);
   }
-  get framework(): AbstractControl<string>{
-    return (<FormControl>this.feForm.get('framework'))
+  get framework(): AbstractControl<string> {
+    return <FormControl>this.feForm.get('framework');
   }
   get hobbies(): AbstractControl<string>[] {
     return (<FormArray>this.feForm.get('hobbies')).controls;
   }
-  get email(): AbstractControl<string>{
-    return (<FormControl>this.feForm.get('email'))
+  get email(): AbstractControl<string> {
+    return <FormControl>this.feForm.get('email');
   }
 }
--- src/app/form/form.service.ts
+++ src/app/form/form.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { FormControl, ValidationErrors } from '@angular/forms';
-import { delay, map, Observable, of, Subject } from 'rxjs';
+import {
+  delay,
+  map,
+  Observable,
+  of,
+  Subject
+} from 'rxjs';
 
 const data: { [key: string]: string[] } = {
   angular: ['1.1.1', '1.2.1', '1.3.3'],
@@ -12,24 +18,20 @@ const data: { [key: string]: string[] } = {
   providedIn: 'root',
 })
 export class FormService {
-  /**subject stream <string[]> with framework versions  */
   frameworkVersions$ = new Subject<string[]>();
 
-  /** accepts framework name as a string and provides the framework's versions to the frameworkVersions$*/
   getFrameworkVersions(framework: string): void {
     of(data)
       .pipe(map((data) => data[framework]))
-      .subscribe((data) => { 
+      .subscribe((data) => {
         this.frameworkVersions$.next(data);
       });
   }
 
-/** accepts a form control and returns an observable<null> if the control's value doesn't equal to 'test@test.test'
- * otherwise returns an observable<ValidationErrors> with 'verifyTest' error
- */
-  asyncValidator(control: FormControl): Observable<ValidationErrors| null> {
+  asyncValidator(control: FormControl): Observable<ValidationErrors | null> {
     return of(control.value).pipe(
       delay(500),
-      map(value => value === 'test@test.test'? {verifyTest: true}  : null))
+      map((value) => (value === 'test@test.test' ? { verifyTest: true } : null))
+    );
   }
 }
