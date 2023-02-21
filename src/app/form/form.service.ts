6883420acf85f76f82c6dae8d0e2d5ac6ceda41d
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2023-02-21 10:51:27 +0200

    added comments to form.service.ts

--- src/app/form/form.service.ts
+++ src/app/form/form.service.ts
@@ -12,17 +12,22 @@ const data: { [key: string]: string[] } = {
   providedIn: 'root',
 })
 export class FormService {
+  /**subject stream <string[]> with framework versions  */
   frameworkVersions$ = new Subject<string[]>();
-  constructor() {}
 
-  getFrameworkVersions(framework: string) {
+  /** accepts framework name as a string and provides the framework's versions to the frameworkVersions$*/
+  getFrameworkVersions(framework: string): void {
     of(data)
       .pipe(map((data) => data[framework]))
-      .subscribe((data) => {
+      .subscribe((data) => { 
         this.frameworkVersions$.next(data);
       });
   }
-   asyncValidator(control: FormControl): Observable<ValidationErrors| null> {
+
+/** accepts a form control and returns an observable<null> if the control's value doesn't equal to 'test@test.test'
+ * otherwise returns an observable<ValidationErrors> with 'verifyTest' error
+ */
+  asyncValidator(control: FormControl): Observable<ValidationErrors| null> {
     return of(control.value).pipe(
       delay(500),
       map(value => value === 'test@test.test'? {verifyTest: true}  : null))
